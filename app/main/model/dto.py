d188c94049b0aae5aa3229fe74dd56bfab8dfee3
mykohl <doleman.michael@gmail.com>
2019-12-07 13:18:43 -0800

    change to dto

--- app/main/model/dto.py
+++ app/main/model/dto.py
@@ -5,4 +5,5 @@ class ItemTypeDto:
     item_type = api.model('item_type', {
         'name': fields.String(description='short name of item type')
         'path': fields.String(description='full hierarchical path to item type node')
+        'root_id': fields.Integer(description='item type id for top-level parent')
     })
